b752fd8189b5db4372e1d20aa76c0f542d86b80c
skanakia <skanakia1@gmail.com>
2018-06-14 14:34:28 -0400

    fixed server paths

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 
+app.use(express.static(__dirname + '/'));
 
 app.get("*", function (req, res) {
     fs.readFile('./index.html', function(error, content) {
