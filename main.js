1e3f785e6c8e8bcbda5fc3fca5b16af056e88848
skanakia <skanakia1@gmail.com>
2018-06-16 03:30:42 -0400

    prettifying

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 
 $(document).ready(function () {
+    var mymap;
 
-
-    $("#submit").on("click", function (event) {
-        event.preventDefault();
+    $("#random").on("click", function (event) {
+        // event.preventDefault();
 
         var origin = $("#origin").val().trim();
         var length = $("#length").val().trim();
@@ -58,6 +58,7 @@ $(document).ready(function () {
                             "votes": results[i].starVotes,
                             "location": results[i].location,
                             "image": results[i].imgSmallMed,
+                            "imageThumb": results[i].imgSqSmall,
                             "length": results[i].length,
                             "ascent": results[i].ascent,
                             "descent": results[i].descent,
@@ -73,17 +74,17 @@ $(document).ready(function () {
                 for (var j = 0; j < trails.length; j++) {
                     var newDiv = $("<div>").attr("class", "newTrail");
                     var trailImage = $("<img>").attr("src", trails[j].image);
-                    var newH4 = $("<h4>").text(trails[j].name).attr({"data-lat": trails[j].latitude, "data-long": trails[j].longitude, "class": "highlight"});
+                    var newH4 = $("<h4>").text(trails[j].name).attr({ "data-lat": trails[j].latitude, "data-long": trails[j].longitude, "class": "highlight" });
                     var newP = $("<p>").text("Summary: " + trails[j].summary);
                     var newP3 = $("<p>").text("Difficulty: " + trails[j].difficulty);
-                    var newP4 = $("<p>").text("Rating: " + trails[j].rating);
+                    var newP4 = $("<p>").text("Rating: " + trails[j].rating + " / 5 stars");
                     var newP5 = $("<p>").text("Votes: " + trails[j].votes);
                     var newP6 = $("<p>").text("Location: " + trails[j].location);
-                    var newP7 = $("<p>").text("Length: " + trails[j].length);
-                    var newP8 = $("<p>").text("Ascent: " + trails[j].ascent);
-                    var newP9 = $("<p>").text("Descent: " + trails[j].descent);
-                    var newP10 = $("<p>").text("High Altitude: " + trails[j].high);
-                    var newP11 = $("<p>").text("Low Altitude: " + trails[j].low);
+                    var newP7 = $("<p>").text("Length: " + trails[j].length + " miles");
+                    var newP8 = $("<p>").text("Ascent: " + trails[j].ascent + " ft");
+                    var newP9 = $("<p>").text("Descent: " + trails[j].descent + " ft");
+                    var newP10 = $("<p>").text("High Altitude: " + trails[j].high + " ft");
+                    var newP11 = $("<p>").text("Low Altitude: " + trails[j].low + " ft");
 
                     $(newDiv).append(newH4);
                     $(newDiv).append(trailImage);
@@ -100,72 +101,63 @@ $(document).ready(function () {
 
                     $("#trail-dump").append(newDiv);
                 }
-                var mymap = L.map('mapid').setView([latitude, longitude], 7);
+                mymap = L.map('mapid').setView([latitude, longitude], 7);
 
                 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-                  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-                  maxZoom: 18,
-                  id: 'mapbox.streets',
-                  accessToken: 'pk.eyJ1IjoiamF5cmVkZDExIiwiYSI6ImNqaGdsaWF3dzFpZjYzZHAzeW4wbHNmb2UifQ.COxlVvDKbzGEnSyy5Um6vg'
+                    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                    maxZoom: 18,
+                    id: 'mapbox.streets',
+                    accessToken: 'pk.eyJ1IjoiamF5cmVkZDExIiwiYSI6ImNqaGdsaWF3dzFpZjYzZHAzeW4wbHNmb2UifQ.COxlVvDKbzGEnSyy5Um6vg'
                 }).addTo(mymap);
 
-//                 var marker = L.marker([38.895469, -77.02733]).addTo(mymap);
-//                 var marker2 = L.marker([38.895769, -77.02333]).addTo(mymap);
-//                 var marker3 = L.marker([38.895769, -77.02333]).addTo(mymap);
-//
-//                 var circle = L.circle([38.895469, -77.02733], {
-//                   color: 'red',
-//                   fillColor: '#f03',
-//                   fillOpacity: 0.5,
-//                   radius: 25
-//                 }).addTo(mymap);
-//
-// var circle2 = L.circle([38.895769, -77.02933], {
-//   color: 'red',
-//   fillColor: '#f03',
-//   fillOpacity: 0.5,
-//   radius: 25
-// }).addTo(mymap);
-//
-// var polygon = L.polygon([
-//   [38.895469, -77.02733],
-//   [38.895469, -77.02733],
-//   [38.895469, -77.02733]
-// ]).addTo(mymap);
+                for (var i = 0; i < trails.length; i++) {
+                    var popup = L.popup({ className: 'popup' })
+                        .setContent('<div class="popupDiv">' +
+                            '<h6>' + trails[i].name + '</h6>' +
+                            '<img src="' + trails[i].imageThumb + '">' +
+                            '<p><b>' + 'Rating: ' + trails[i].rating + ' / 5 stars'+ '</b><br>' +
+                            '<b>' + 'Length: ' + trails[i].length + ' miles' + '</b><br>' +
+                            'Difficulty: ' + trails[i].difficulty +
+                            '</p>' +
+                            '</div>');
 
-// marker.bindPopup("Fogo de Chao").openPopup();
-// marker2.bindPopup("Trail Zamunda").openPopup();
-// circle.bindPopup("Fogo de Chao");
-// circle2.bindPopup("Fogo de Chao");
-// polygon.bindPopup("Fogo de Chao");
-   for (var i = 0; i < trails.length; i++) {
-           marker = new L.marker([trails[i].latitude,trails[i].longitude])
-               .bindPopup(trails[i].name)
-               .addTo(mymap);
-             }
+                    marker = new L.marker([trails[i].latitude, trails[i].longitude])
+                        .bindPopup(popup)
+                        .addTo(mymap);
+                }
 
 
-function onMapClick(e) {
-  alert("You clicked the map at " + e.latlng);
-}
+                // function onMapClick(e) {
+                //     alert("You clicked the map at " + e.latlng);
+                // }
 
-mymap.on('click', onMapClick);
+                // mymap.on('click', onMapClick);
 
-var popup = L.popup();
+                // var popup = L.popup();
 
-function onMapClick(e) {
-  popup
-    .setLatLng(e.latlng)
-    .setContent("You clicked the map at " + e.latlng.toString())
-    .openOn(mymap);
-}
-
-mymap.on('click', onMapClick);
+                // function onMapClick(e) {
+                //     popup
+                //         .setLatLng(e.latlng)
+                //         .setContent("You clicked the map at " + e.latlng.toString())
+                //         .openOn(mymap);
+                // }
+                
+                // mymap.on('click', onMapClick);
             });
         });
 
     });
 
+    $(document).on('mouseenter', '.highlight', function() {
+        $(this).css("color", "purple");
+    });
 
+    $(document).on('mouseleave', '.highlight', function() {
+        $(this).css("color", "black");
+    });
 
+    $(document).on('click', '.highlight', function() {
+        mymap.panTo([$(this).data("lat"), $(this).data("long")]);
+        mymap.fitBounds(L.latLngBounds([[$(this).data("lat") - .025, $(this).data("long") - .025], [$(this).data("lat") + .025, $(this).data("long") + .025]]));
+    });
 });
