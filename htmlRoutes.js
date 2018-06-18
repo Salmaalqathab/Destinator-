701db590e2827f6b74f1eaf6cee5982d15c23a97
marcpsalazar@gmail.com <Patrick@Traceys-MacBook.local>
2018-06-18 14:29:49 -0400

    Second

--- htmlRoutes.js
+++ htmlRoutes.js
@@ -4,20 +4,12 @@ var path = require("path");
 module.exports = function(app) {
 
 
-  app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "../survey.html"));
-  });
-
-  app.get("/results", function(req, res) {
-    res.sendFile(path.join(__dirname, "../results.html"));
-  });
-
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../home.html"));
+    res.sendFile(path.join(__dirname, "./index.html"));
   });
 
   // If no matching route is found default to home
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../home.html"));
+    res.sendFile(path.join(__dirname, "./index.html"));
   });
 };
